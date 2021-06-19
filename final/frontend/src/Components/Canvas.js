812b37d443748c2f458180c28222c75732865ef1
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-06-19 17:21:44 +0800

    add canvas toolbar & new function

--- final/frontend/src/Components/Canvas.js
+++ final/frontend/src/Components/Canvas.js
@@ -1,10 +1,15 @@
 import React, { useRef, useEffect, useState } from 'react'
+import pencil from "../Images/pencil.png"
+import eraser from "../Images/eraser.png"
+import broom from "../Images/broom.png"
 import "../App.css"
 
 const Canvas = () => {
     const canvasRef = useRef(null)
     const contextRef = useRef(null)
     const [isDrawing, setIsDrawing] = useState(false)
+    const [mode, setMode] = useState('pencil')
+    const [color, setColor] = useState('black')
     const handleMouseMove = (e)=>{
         if(!isDrawing){
             return
@@ -12,20 +17,29 @@ const Canvas = () => {
         const {pageX, pageY} = e
         const offsetX = canvasRef.current.offsetLeft
         const offsetY = canvasRef.current.offsetTop
-        contextRef.current.lineTo((pageX-offsetX)/2, (pageY-offsetY)/2)
-        contextRef.current.stroke()
+        if(mode === 'pencil'){
+            contextRef.current.lineTo((pageX-offsetX)/2, (pageY-offsetY)/2)
+            contextRef.current.stroke()
+        }else if(mode === 'eraser'){
+            contextRef.current.clearRect((pageX-offsetX)/2, (pageY-offsetY)/2, 5, 5)
+        }
     }
     const handleMouseDown = (e)=>{
         const {pageX, pageY} = e
         contextRef.current.beginPath()
         const offsetX = canvasRef.current.offsetLeft
         const offsetY = canvasRef.current.offsetTop
-        contextRef.current.lineTo((pageX-offsetX)/2, (pageY-offsetY)/2)
+        if(mode === 'pencil'){
+            contextRef.current.lineTo((pageX-offsetX)/2, (pageY-offsetY)/2)
+        }
         setIsDrawing(true)
     }
     const handleMouseUp = (e)=>{
         contextRef.current.closePath()
         setIsDrawing(false)
+    }
+    const handleChange = (e)=>{
+        setColor(e.target.value)
     }
     useEffect(()=>{
         const canvas = canvasRef.current;
@@ -36,13 +50,21 @@ const Canvas = () => {
         const context = canvas.getContext("2d")
         context.scale(2,2)
         context.lineCap = "round"
-        context.strokeStyle = 'black'
+        context.strokeStyle = color
         context.lineWidth = 1;
         contextRef.current = context
     },[])
-  
+    useEffect(()=>{
+        contextRef.current.strokeStyle = color
+    }, [color])
     return (
         <div className='Canvas'>
+        <div className='toolbar'>
+        <img src={pencil} width="30" height="30" onClick={()=>{setMode('pencil')}}/>
+        <img src={eraser} width="30" height="30" onClick={()=>{setMode('eraser')}}/>
+        <img src={broom} width="30" height="30" onClick={()=>{contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)}} />
+        <input type="color" id="color_picker" onChange={handleChange}/>
+        </div>
         <canvas
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
