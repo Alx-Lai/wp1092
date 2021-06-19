085dfae449c991f159b066677087c76970fc7d2a
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-06-19 16:04:28 +0800

    fix canvas bug

--- final/frontend/src/Components/Canvas.js
+++ final/frontend/src/Components/Canvas.js
@@ -1,8 +1,6 @@
-
 import React, { useRef, useEffect, useState } from 'react'
 import "../App.css"
 
-const r = 5
 const Canvas = () => {
     const canvasRef = useRef(null)
     const contextRef = useRef(null)
@@ -11,15 +9,18 @@ const Canvas = () => {
         if(!isDrawing){
             return
         }
-        const {offsetX, offsetY} = e
-        contextRef.current.lineTo(offsetX, offsetY)
+        const {pageX, pageY} = e
+        const offsetX = canvasRef.current.offsetLeft
+        const offsetY = canvasRef.current.offsetTop
+        contextRef.current.lineTo((pageX-offsetX)/2, (pageY-offsetY)/2)
         contextRef.current.stroke()
-        console.log('draw')
     }
     const handleMouseDown = (e)=>{
-        const {offsetX, offsetY} = e
+        const {pageX, pageY} = e
         contextRef.current.beginPath()
-        contextRef.current.moveTo(offsetX,offsetY)
+        const offsetX = canvasRef.current.offsetLeft
+        const offsetY = canvasRef.current.offsetTop
+        contextRef.current.lineTo((pageX-offsetX)/2, (pageY-offsetY)/2)
         setIsDrawing(true)
     }
     const handleMouseUp = (e)=>{
@@ -28,24 +29,27 @@ const Canvas = () => {
     }
     useEffect(()=>{
         const canvas = canvasRef.current;
-        //canvas.width = window.innerWidth * 2;
-        //canvas.height = window.innerHeight * 2;
-        //canvas.style.width = `${window.innerWidth}px` ;
-        //canvas.style.height = `${window.innerHeight}px`;
+        canvas.width = 626.670;
+        canvas.height = 374;
+        canvas.style.width = `${canvas.width}px`;
+        canvas.style.height = `${canvas.height}px`;
         const context = canvas.getContext("2d")
         context.scale(2,2)
         context.lineCap = "round"
         context.strokeStyle = 'black'
-        context.lineWidth = 5;
+        context.lineWidth = 1;
         contextRef.current = context
     },[])
   
     return (
-        <canvas ref={canvasRef}
+        <div className='Canvas'>
+        <canvas
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
             onMouseMove={handleMouseMove}
+            ref={canvasRef}
         />
+        </div>
     )
 }
 
