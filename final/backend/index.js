8c39eb6271c0e658241b2a9129bfb12aa9e30002
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-06-23 13:50:14 +0800

    backend frame

---
+++ final/backend/index.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const http = require('http');
+const WebSocket = require('ws');
+const express = require('express');
+const path = require('path');
+const uuid = require('uuid');
+
+const mongo = require('./mongo');
+
+const app = express();
+
+//get MONGO_URL
+require('dotenv-defaults').config();
+/* -------------------------------------------------------------------------- */
+/*                               MONGOOSE MODELS                              */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                  UTILITIES                                 */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                            SERVER INITIALIZATION                           */
+/* -------------------------------------------------------------------------- */
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({
+  server,
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+wss.on('connection', function connection(client) {
+  client.id = uuid.v4();
+
+  client.on('message', async function incoming(message) {
+    message = JSON.parse(message);
+
+    // disconnected
+    client.once('close', () => {
+      chatBoxes[client.box].delete(client);
+    });
+  });
+});
+
+mongo.connect();
+
+server.listen(8080, () => {
+  console.log('Server listening at http://localhost:8080');
+});
