30c15b8dd161be0354075e2a2b688a269c793279
leyun <b09902088@csie.ntu.edu.tw>
2021-06-24 15:12:53 +0800

    message demo

--- final/frontend/src/Containers/Room.js
+++ final/frontend/src/Containers/Room.js
@@ -5,6 +5,7 @@ import UserList from '../Components/UserList';
 import {Input} from "antd";
 import useGame from "../Hooks/useGame";
 import Waiting from './Waiting';
+import {CheckCircleTwoTone} from '@ant-design/icons';
 const Room = ({me, info, displayStatus})=>{ 
     const [users, setusers] = useState([me]); 
     const [usernum, setusernum] = useState(1);
@@ -12,10 +13,15 @@ const Room = ({me, info, displayStatus})=>{
     const [gamestart, setgamestart] = useState(false);
     const [displayText, setdisplayText] = useState("gartic");
     const [isdrawing, setisdrawing] = useState(false);
+    const [canGuess, setcanGuess] = useState(false);
+    const [messages, setmessages] = useState([{sender:{name: "alex"}, body:"apple", correct:false},{sender:{name: "alex"}, body:"apple", correct:false}, {sender:{name: "alex", _id:""}, body:"alex guessed!", correct:true}]);
     useEffect(() => {
       if(status.type == "START"){ //{type:"START"}
         // setStart(true)
         setgamestart(true);
+      }
+      if(status.type == "MYID"){
+        me._id = status.data.id;
       }
       if(status.type == "JOIN"){ 
         setusers([...users, {name: status.data.userList.name, me: false, score: status.data.userList.score, color: status.data.userList.color, _id: status.data.userList._id}])
@@ -47,8 +53,14 @@ const Room = ({me, info, displayStatus})=>{
           {(isdrawing)? <Canvas />: <div className="Canvas">{displayText}</div>}
           <Waiting num={usernum} visible={!gamestart} />
           <div className = "Chat">
-            <div className="Chat-messages"></div>
-            <Input.Search id="searchBar" placeholder="guess here..." enterButton="send"></Input.Search>
+            <div className="Chat-messages">{messages.map(n=>(
+              (n.correct)? <>
+                {/* <CheckCircleTwoTone twoToneColor="#52c41a" /> */}
+                <p className="correctFont">{(n.sender._id==me._id)?'You guessed!':n.body}</p>
+              </>
+              :<p className="guessFont">{n.sender.name}: {n.body}</p>
+        ))}</div>
+            <Input.Search id="searchBar" placeholder="guess here..." enterButton="send" disabled={!canGuess}></Input.Search>
           </div>
         </div>
       </div>
